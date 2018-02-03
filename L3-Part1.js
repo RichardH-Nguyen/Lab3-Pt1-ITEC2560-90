8a541873f110af82d0be1c62fcb4c268e10dacbf
RHN <stuff@stuff.com>
2018-02-03 16:31:47 -0600

    Part1b done

--- L3-Part1.js
+++ L3-Part1.js
@@ -2,19 +2,33 @@
 document.getElementById("addItem").addEventListener("click", addToList);
 //Hitting enter on the keyboard adds the string in the textbox to the list.
 document.getElementById("newListItem").addEventListener("keyup", enterBtnAdd);
-//Putting focus on the textarea clears it.
-document.getElementById("newListItem").addEventListener("focus", clearOnFocus);
+//Putting focus on the text area clears it.
+//document.getElementById("newListItem").addEventListener("focus", clearOnFocus);
 
 function addToList() {
     var myList = document.getElementById("mylist");
     var newItem = document.createElement("li");
     var textBoxString = document.getElementById("newListItem").value;
-    newItem.innerHTML = textBoxString;
-    myList.appendChild(newItem);
-    
+
+    //If textbox is empty or user hits enter on an empty textbox, return.
+    if(textBoxString === "" || textBoxString === "\n"){
+        return;
+    }
+    else if(textBoxString.length > 50){
+        alert("List items can only contain 50 characters or less.");
+        return;
+    }
+    //Otherwise add to list.
+    else {
+        newItem.innerHTML = textBoxString;
+        myList.appendChild(newItem);
+        textBoxString = "";
+    }
+
     document.getElementById("newListItem").focus();
 }
 
+//listener for clearOnFocus() currently commented out. As requested to not clear input after alert message.
 function clearOnFocus() {
     document.getElementById("newListItem").value ="";
 
@@ -25,5 +39,6 @@ function enterBtnAdd() {
     if (event.keyCode === 13){
         addToList();
         clearOnFocus();
+
     }
 }
